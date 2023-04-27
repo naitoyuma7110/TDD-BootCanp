94a500a02d19b7392f2f4eccf0358a6f50f3a3ef
Naito Yuma <naitoyuma1118@gmail.com>
2023-04-27 14:20:44 +0900

    テストコードをドキュメントと一致させる
    コメント：
    講義はここまでとなっているがテストの完成のためには
    「5と3の倍数の場合はFizzBuzzに変換する」テストが必要

--- src/main/java/com/example/FizzBuzz.java
+++ src/main/java/com/example/FizzBuzz.java
@@ -2,8 +2,15 @@ package com.example;
 
 public class FizzBuzz {
 
-  public String convert(int i) {
-    return null;
+  public String convert(int num) {
+    if (num == 3) {
+      return "Fizz";
+    }
+    if (num == 5) {
+      return "Buzz";
+    }
+    
+    return String.valueOf(num);
   }
 
 }
--- src/test/java/com/example/FizzBuzzTest.java
+++ src/test/java/com/example/FizzBuzzTest.java
@@ -1,20 +1,60 @@
 package com.example;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
+
+@DisplayName("Fizz,Buzz,数列と変換規則を扱うFizzBuzzクラス")
 public class FizzBuzzTest {
+  private FizzBuzz fizzbuzz;
 
-  @Test
-  public void _1を渡すと文字列1を返す() throws Exception {
-    // 準備
-    FizzBuzz fizzbuzz = new FizzBuzz();
-    // 実行
-    String actual = fizzbuzz.convert(1);
-    // 検証
-    assertEquals("失敗しました", "1", actual);
-
+  // 各テスト共通の準備
+  @BeforeEach
+  void 事前準備() {
+    System.out.println("事前準備");
+    // 変数名の重複がない限りthisを省略する事で,自動でクラスフィールドを検索し解決してくれる
+    fizzbuzz = new FizzBuzz();
   }
+
+  @Nested
+  class convertメソッドは数を文字列に変換する {
+  
+  @Nested
+  class _3で割り切れる場合はFizzと変換 {
+    @Test
+    public void _3を渡すと文字列Fizzを返す() throws Exception {
+      // 実行・検証
+      assertEquals("Fizz", fizzbuzz.convert(3));
+    }
+  }
+  
+  @Nested
+  class _5で割り切れる場合はFizzと変換 {
+    @Test
+    public void _5を渡すと文字列Buzzを返す() throws Exception {
+      // 実行・検証
+      assertEquals("Buzz", fizzbuzz.convert(5));
+    }
+  }
+  
+  @Nested
+  class その他の数のときはそのまま文字列に変換する {
+      
+    @Test
+    public void _1を渡すと文字列1を返す() throws Exception {
+    // 実行・検証
+    assertEquals( "1", fizzbuzz.convert(1));
+    }
+    
+    @Test
+    public void _2を渡すと文字列2を返す() throws Exception {
+      // 実行・検証
+      assertEquals("2", fizzbuzz.convert(2));
+    }
+  }
+}
   
 }
